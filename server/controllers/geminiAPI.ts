04210f521b2eefad209eb2d39be1f317838d681c
Varun Singh <varun7singh10@gmail.com>
2024-04-26 02:23:58 +0530

    feat: added express routes for gemini

---
+++ server/controllers/geminiAPI.ts
@@ -0,0 +1,19 @@
+import type { Request, Response } from "express";
+import { model } from "../server";
+
+export const geminiAPI = async (req: Request, res: Response) => {
+  const { code, text } = req.body;
+  try {
+    const response = await model.getResponse(code, text);
+    res.status(200).json({
+      status: "success",
+      message: response,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: "error",
+      message: error,
+    });
+  }
+};
