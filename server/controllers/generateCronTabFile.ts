1079fb30677cbed4895995749e582d58ca70e28e
sammybh6 <samyakbhargava9@gmail.com>
2024-04-26 02:51:57 +0530

    Added cron tab file generatiom inn api

--- server/controllers/generateCronTabFile.ts
+++ server/controllers/generateCronTabFile.ts
@@ -1,5 +1,7 @@
 import type { Request, Response } from 'express';
 import { z } from 'zod';
+const fs = require('fs');
+const { exec } = require('child_process');
 
 interface Medicine {
     minutes: number;
@@ -17,7 +19,7 @@ export const generateCronTabFile = (req: Request, res: Response) => {
         minutes: z.number().min(0, {message: "Minutes should be greater than 0"}).max(59, {message: "Minutes should be less than 59"}),
         hours: z.number().min(0, {message: "Hours should be greater than 0"}).max(23, {message: "Hours should be less than 23"}),
         days: z.array(z.number().min(0, {message: "Days should be greater than 0"}).max(6, {message: "Days should be less than 6"})),
-        dates: z.array(z.number().min(0, {message: "Dates should be greater than 0"}).max(31, {message: "Days should be less than 31"})),
+        dates: z.array(z.number().min(1, {message: "Dates should be greater than 0"}).max(31, {message: "Days should be less than 31"})),
         medicine: z.string(),
     }).array();
 
@@ -56,7 +58,19 @@ export const generateCronTabFile = (req: Request, res: Response) => {
 
     // send the response in the form of a file
     res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Content-Disposition', 'attachment; filename=cronTabFile.txt');
     res.send(cronTabFile);
+
+
+    exec('echo "' + cronTabFile + '" > cronTab.txt', (error: any, stdout: any, stderr: any) => {
+        if (error) {
+            console.error(`Error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`File content written successfully to cronTab.txt`);
+    });
 }
 
