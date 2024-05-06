36c948953203c7c6989bca0e40d202d7ad0f7fd9
sammybh6 <samyakbhargava9@gmail.com>
2024-05-06 14:54:49 +0530

    Removed zod validation

--- server/controllers/generateCronTabFile.ts
+++ server/controllers/generateCronTabFile.ts
@@ -1,7 +1,4 @@
 import type { Request, Response } from 'express';
-import { z } from 'zod';
-const fs = require('fs');
-const { exec } = require('child_process');
 
 interface Medicine {
     minutes: number;
@@ -15,20 +12,6 @@ export const generateCronTabFile = (req: Request, res: Response) => {
     const { medicines } = req.body;
     // const { minutes, hours, days, dates, medicine } = req.body;
     // Validate the request body
-    const schema = z.object({
-        minutes: z.number().min(0, {message: "Minutes should be greater than 0"}).max(59, {message: "Minutes should be less than 59"}),
-        hours: z.number().min(0, {message: "Hours should be greater than 0"}).max(23, {message: "Hours should be less than 23"}),
-        days: z.array(z.number().min(0, {message: "Days should be greater than 0"}).max(6, {message: "Days should be less than 6"})),
-        dates: z.array(z.number().min(1, {message: "Dates should be greater than 0"}).max(31, {message: "Days should be less than 31"})),
-        medicine: z.string(),
-    }).array();
-
-    try {
-        schema.parse(medicines);
-    } catch (error) {
-        res.status(400).send({ error: "Invalid request body" });
-        return;
-    }
     let cronTabFile = "";
     medicines.forEach((med: Medicine) => {
         const { minutes, hours, days, dates, medicine } = med;
